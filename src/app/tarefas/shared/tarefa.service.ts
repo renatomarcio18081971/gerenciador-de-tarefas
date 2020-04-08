4f71f7a8a77f3d1b7b46aef9b8e6681cd4242cac
renatomarcio18081971 <renato.marcio@gmail.com>
2020-04-08 14:18:35 -0300

    inclusao

---
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from './';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas): []
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.Id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(t => t.Id === id);
+  }
+
+}
